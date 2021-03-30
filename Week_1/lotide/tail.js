54e125641a7fa62f014b1ef29ce7083ebe60755d
Matt Macdonald <mksmacdonald@gmail.com>
2021-03-30 00:15:16 +0000

    This is tail

---
+++ Week_1/lotide/tail.js
@@ -0,0 +1,35 @@
+// FUNCTION IMPLEMENTATION
+const tail = function(dataArray) {
+  let dataTail = [];
+
+  if (!dataArray) {
+    return undefined;
+  }
+  for (let i = 1; i <= dataArray.length - 1; i++) {
+    dataTail.push(dataArray[i]);
+  }
+
+  return dataTail;
+};
+
+const checkTail = function(dataArray, expectedOutput) {
+  let dataTail = tail(dataArray);
+
+  for (let i = 0; i <= dataArray - 2; i++) {
+    let samsies = assertEqual(dataTail[i], expectedOutput[i]);
+
+    if (!samsies) {
+      console.log(`Assertion failed: ${dataArray} !== ${expectedOutput}`);
+
+      return null;
+    }
+  }
+
+  console.log(`Assertion Passed: ${dataArray} === ${expectedOutput}`);
+};
+
+const assertEqual = function(actual, expected) {
+  return actual === expected;
+};
+
+checkTail(["Yo Yo", "Lighthouse", "Labs"], ["Lighthouse", "Labs"]);
