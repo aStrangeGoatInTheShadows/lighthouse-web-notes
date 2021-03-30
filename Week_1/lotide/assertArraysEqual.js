a901b64ea43d9200f551268645089218ff1c9543
Matt Macdonald <mksmacdonald@gmail.com>
2021-03-30 02:46:14 +0000

    Added assertArraysEqual.js

---
+++ Week_1/lotide/assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(ar1, ar2) {
+  if (ar1.length !== ar2.length) {
+    return false;
+  }
+
+  for (let i = 0; i <= ar1.length; i++) {
+    if (ar1[i] !== ar2[i]) {
+      return false;
+    }
+  }
+  
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
